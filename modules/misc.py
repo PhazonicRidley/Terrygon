707437d8db2e4a0c6e4cbc21b12b3de861685d63
46620 <46620osu@gmail.com>
2022-06-12 12:03:03 -0400

    [fix] default skins can now default dance

--- modules/misc.py
+++ modules/misc.py
@@ -92,7 +92,7 @@ class Misc(commands.Cog):
             embed = discord.Embed(title=f'**Userinfo for {user.name}#{str(user.discriminator)}**',
                                   color=user.color.value)
             embed.description = f"""**User's ID:** {str(user.id)} \n **Join date:** {str(user.joined_at)} \n**Created on** {str(user.created_at)}\n **Current Status:** {str(user.status).upper() if str(user.status).lower() == "dnd" else str(user.status).title()}\n **User Activity:**: {str(user.activity)} \n **Default Profile Picture:** {str(user.default_avatar).title()}\n **Current Display Name:** {user.display_name}\n**Nitro Boost Date:** {str(user.premium_since)}\n **Current Top Role:** {str(user.top_role)}\n **Bot** {user.bot}\n **Color:** {str(hex(user.color.value)[2:]).zfill(6)}"""
-            embed.set_thumbnail(url=user.avatar.url)
+            embed.set_thumbnail(url=user.display_avatar.url)
             await ctx.send(embed=embed)
 
         elif not in_server:
@@ -105,7 +105,7 @@ class Misc(commands.Cog):
                                   color=common.gen_color(user.id))
             embed.description = f"**User's ID:** {str(user.id)} \n **Default Profile Picture:** {str(user.default_avatar)} \n  **Created on:** {str(user.created_at)}\n **Bot:** {user.bot}\n {f'**Banned, reason:** {ban.reason}' if ban is not None else ''}"
             embed.set_footer(text=f'{user.name}#{user.discriminator} is not in your server.')
-            embed.set_thumbnail(url=user.avatar.url)
+            embed.set_thumbnail(url=user.display_avatar.url)
             await ctx.send(embed=embed)
 
     @commands.command(aliases=['avi'])
